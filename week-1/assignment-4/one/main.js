4a34ef0c5dfbffe192b0585226f26e1f15a0b162
omnia tawfek <148563607+omniatawfek@users.noreply.github.com>
2025-02-25 19:48:50 +0200

    Add files via upload

---
+++ week-1/assignment-4/one/main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(1e5); // 100000
+console.log(10**5); // 100000
+console.log(1_000_00); // 100000
+console.log(1000*100); // 100000
+console.log(2e5-1e5); // 100000
+console.log(50000+50000); // 100000
+console.log(Math.round(99999.7)); // 100000
+console.log(Math.ceil(99999.4)); // 100000
+console.log(Math.floor(100000.8)); // 100000
+console.log(Math.trunc(100000.7)); // 100000
